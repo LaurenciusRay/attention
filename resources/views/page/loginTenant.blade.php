6cc9d9760ebd2538310431e99c3268e1c1e63c84
mRizalMaulana <mrizalmaulana000@yahoo.com>
2019-10-14 18:05:06 +0700

    create form login for EO and Tnt

--- resources/views/page/loginTenant.blade.php
+++ resources/views/page/loginTenant.blade.php
@@ -1,23 +1,27 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-
-</html>
+@section('content')
+<div class="container">
+    <div class="col-md-6 col-lg-5 col-xl-5 mx-auto my-5">
+        <div class="rounded" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+            <div class="bg-primary px-5 pt-5 pb-3 border border-primary rounded-top" style="background: -webkit-gradient(linear, left top, right top, from(#5e02f5), to(#3e0b91));
+                    background: linear-gradient(to right, #5e02f5, #3e0b91); color:#fff;">
+                <h3>Tenant<br>Login</h3>
+            </div>
+            <div class="px-5 pb-5">
+                <form action="">
+                    <div class="form-group mt-3">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" placeholder="your email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" placeholder="your password">
+                    </div>
+                    <button class="btn btn-primary w-100 mt-1">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
