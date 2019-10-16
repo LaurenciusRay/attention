ca2f257d9dca85f440a0f3633404c3780f2590fd
mRizalMaulana <mrizalmaulana000@yahoo.com>
2019-10-16 23:38:34 +0700

    update TenantUser model

--- app/Tenant/User/TenantUser.php
+++ app/Tenant/User/TenantUser.php
@@ -2,9 +2,22 @@
 
 namespace App\Tenant\User;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class TenantUser extends Model
+class TenantUser extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $fillable = [
+        'name',
+        'email', 
+        'image_banner',
+        'password',
+        'description',
+    ];
 }
