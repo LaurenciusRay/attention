38615e836427390dd215a8b47585249e10dc74af
LaurenciusRay <raymondantonio587@gmail.com>
2019-10-18 09:45:55 +0700

    view_gallery

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -9,9 +9,17 @@
                 </div>
                 <div class="col-sm-6">
                     <div class="row d-flex justify-content-end">
-                        <button class="btn btn-danger btn-sm">
+                        <button href="{{ asset('storage/events/photo-1521358522799-ad7a75d9e346.jpg') }}" class="btn btn-danger btn-sm" data-fancybox="images-preview" data-thumbs='{"autoStart":true}'>
                             View Gallery
                         </button>
+                        <div style="display:none;">
+                            <a href="{{ asset('storage/events/photo-1506112573664-1a1b66d93ff3.jpg') }}" data-fancybox="images-preview"
+                            data-width="1500" data-height="1000"
+                            ></a>
+                            <a href="{{ asset('storage/events/photo-1496304841270-2cb66cf766b4.jpg') }}" data-fancybox="images-preview"
+                            data-width="1500" data-height="1000"
+                            ></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -28,23 +36,6 @@
                 <div class="row mb-2">
                     <div class="card px-3">
                         <p> {!! $event->description !!} </p>
-                    </div>
-                </div>
-                
-                <div class="row mb-5 text-light">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="{{ asset('storage/events/photo-1521358522799-ad7a75d9e346.jpg') }}" alt="" width="100%">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="{{ asset('storage/events/photo-1506112573664-1a1b66d93ff3.jpg') }}" alt="" width="100%">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="{{ asset('storage/events/photo-1496304841270-2cb66cf766b4.jpg') }}" alt="" width="100%">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="{{ asset('storage/events/photo-1521358522799-ad7a75d9e346.jpg') }}" alt="" width="100%">
-                        </div>
                     </div>
                 </div>
                 <div class="row mb-3 d-flex justify-content-center">
@@ -118,6 +109,7 @@
 @endsection
 @section('css_link')
     <link href="{{asset('css/events/style.css')}}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
 @endsection
 @section('css_script')
     <style>
@@ -129,6 +121,7 @@
     </style>
 @endsection
 @section('script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
     <script>
         function handleRegister(id)
         {
