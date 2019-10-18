3718970160cbec1da6509d4ac352acb0ce0d568e
LaurenciusRay <raymondantonio587@gmail.com>
2019-10-18 10:03:17 +0700

    change login-eo-user-form to login-eo-user

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::namespace('Frontend\Auth\Regist')->name('regist.')->group(function () {
 });
 
 Route::namespace('Frontend\Auth\Login')->name('login.')->group(function () { 
-    Route::get('/login-eo', 'EoLoginController@formLoginEo')->name('eo-user-form');
+    Route::get('/login-eo', 'EoLoginController@formLoginEo')->name('eo-user');
     Route::get('/login-tenant', 'TenantLoginController@formLoginTenant')->name('tenant-user-form');
 });
