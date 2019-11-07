2f6c5cd6e5f659da929d8c2b5f61b9d233c98437
Muhamar <amararrasyid@gmail.com>
2019-11-07 11:30:02 +0700

    nothing

--- resources/views/tenant/show.blade.php
+++ resources/views/tenant/show.blade.php
@@ -55,11 +55,6 @@
                                 <td>:</td>
                                 <th>Pedagang (Sepatu Import)</th>
                             </tr>
-                            <tr>
-                                <td>Tahun Mulai Berdagang</td>
-                                <td>:</td>
-                                <th>1991</th>
-                            </tr>
                         </table>
                     </p>
                 </div>
@@ -93,7 +88,49 @@
                             </button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <form>
+                                <div class="form-group row">
+                                    <label for="inputName" class="col-sm-2 col-form-label">Nama</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="inputEmail3" placeholder="nama">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputjk" class="col-sm-2 col-form-label">Jenis Kelamin</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Jenis kelamin">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputTmptLhr" class="col-sm-2 col-form-label">Tempat Lahir</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Tempat Lahir">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputTglLhr" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                                    <div class="col-sm-10">
+                                        <input type="date" class="form-control" id="inputEmail3" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-10">
+
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -135,7 +172,7 @@
                             </button>
                         </div>
                         <div class="modal-body">
-                            ...
+                            ....
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
